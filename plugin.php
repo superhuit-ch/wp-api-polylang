45ca764dcf24a9e42a787190a4ebf10079c6b5a5
Lucas Freitas <lucas@lucasfreitas.com.br>
2017-08-09 13:14:57 +0200

    Update plugin.php
    Allow fetching the term translations

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Description: Polylang integration for the WP REST API
  * Author: Jorge R Garcia / Lucas Freitas
  * Author URI:
- * Version: 0.0.4
+ * Version: 0.0.5
  * Plugin URI:
  * License: MIT
  */
@@ -41,12 +41,37 @@ function polylang_json_api_languages()
     return pll_languages_list();
 }
 
+
+/**
+ * Get the related posts translations (only the permalinks)
+ */
 function polylang_json_api_post_translations($request)
 {
-    return pll_get_post_translations($request['id']);
+    $r = [];
+    foreach (pll_get_post_translations($request['id']) as $lang => $post_id) {
+        $r[$lang] = get_permalink($post_id);
+    }
+    return $r;
+}
+
+/**
+ * Get the post data in a specific language.
+ */
+function polylang_json_api_other_post($request)
+{
+    return pll_get_post($request['id'], $request['lang']);
+}
+
+function polylang_json_api_term_translations($request) {
+    $r = [];
+    foreach (pll_get_term_translations($request['id']) as $lang => $term_id) {
+        $r[$lang] = get_permalink($term_id);
+    }
+    return $r;
 }
 
 add_action('rest_api_init', 'polylang_json_api_init');
+
 add_action('rest_api_init', function () {
     register_rest_route('polylang/v2', '/languages', array(
         'methods' => 'GET',
@@ -62,6 +87,15 @@ add_action('rest_api_init', function () {
                     return is_numeric( $param );
                 }
             )
+        )
+    ));
+    register_rest_route('polylang/v2', '/term/(?P<id>\d+)', array(
+        'methods' => 'GET',
+        'callback' => 'polylang_json_api_term_translations',
+        'args' => array(
+            'id' => array(
+                'validate_callback' => 'is_numeric'
+            ),
         )
     ));
 });
